6b9d93e4b8800f17495b75cf1e304b7cfcf97d79
donkennie <ajeigbekehinde160@gmail.com>
2022-08-10 04:20:09 +0100

    Install a package

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
-/*const slugify = require("slugify");
+const slugify = require("slugify");
 const replaceTemplate = require("./modules/replaceTemplate");
-*/
+
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 
 // console.log(textIn);
@@ -29,6 +29,26 @@ const replaceTemplate = require("./modules/replaceTemplate");
 // });
 // console.log('Will read file!');
 
+// SERVER
+const tempOverview = fs.readFileSync(
+  `${__dirname}/templates/template-overview.html`,
+  "utf-8"
+);
+const tempCard = fs.readFileSync(
+  `${__dirname}/templates/template-card.html`,
+  "utf-8"
+);
+const tempProduct = fs.readFileSync(
+  `${__dirname}/templates/template-product.html`,
+  "utf-8"
+);
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map((el) => slugify(el.productName, { lower: true }));
+console.log(slugs);
+
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
 
@@ -37,6 +57,12 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
       "Content-type": "text/html",
     });
+
+    const cardsHtml = dataObj
+      .map((el) => replaceTemplate(tempCard, el))
+      .join("");
+    const output = tempOverview.replace("{%PRODUCT_CARDS%}", cardsHtml);
+    res.end(output);
 
     // Product page
   } else if (pathname === "/product") {
@@ -63,11 +89,6 @@ const server = http.createServer((req, res) => {
     res.end("<h1>Page not found!</h1>");
   }
 });
-
-//SERVER
-/*const server = http.createServer((req, res) => {
-  res.end("Hello from the server!");
-});*/
 
 server.listen(8000, "127.0.0.1", () => {
   console.log("Listening to requests on port 8000");
